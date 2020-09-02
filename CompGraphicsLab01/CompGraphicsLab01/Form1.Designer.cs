d3d9efd91748c1b72820b9216fe5808a59760a81
Mikhail <mikes22@yandex.ru>
2020-09-02 22:51:42 +0300

    Add Lab01

---
+++ CompGraphicsLab01/CompGraphicsLab01/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CompGraphicsLab01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(38, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "-5";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(38, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "   ";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CompGraphicsLab01/CompGraphicsLab01/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompGraphicsLab01
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        public void DrawGraphic(Func<double,double> func, int lowBound, int upBound)
+        {
+            g.Clear(this.BackColor);
+            g.Dispose();
+            g = this.CreateGraphics();
+            
+            var OY = (int)((-lowBound / (double)(upBound - lowBound)) * g.VisibleClipBounds.Width);
+            g.DrawLine(Pens.Red, OY, 0, OY, g.VisibleClipBounds.Height);
+            var step = g.VisibleClipBounds.Width/(upBound-lowBound);
+            var points = new Tuple<int, double>[(int)g.VisibleClipBounds.Width];
+            for (var i = 0; i< g.VisibleClipBounds.Width; i++)
+            {
+                points[i] = new Tuple<int, double>(i, - func(lowBound + ((double)i / step)));
+            }
+            var min = points.Min(p => p.Item2);
+            var max = points.Max(p => p.Item2);
+            var stepV = g.VisibleClipBounds.Height / (max - min);
+
+            var OX = (int)((-min / (double)(max - min)) * g.VisibleClipBounds.Height);
+            g.DrawLine(Pens.Red, 0, OX, g.VisibleClipBounds.Width, OX);
+
+            for (int i1 = 1; i1 < points.Length; i1++)
+            {
+                Tuple<int, double> p = points[i1];
+                g.DrawLine(Pens.Black, points[i1-1].Item1, (int)(OX + points[i1-1].Item2 * stepV), points[i1].Item1, (int)(OX + points[i1].Item2*stepV));
+            }
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            DrawGraphic(Math.Sin, -5, 5);
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new string[] { "Sin", "x^2", "x^3"});
+            comboBox1.SelectedIndex = 0;
+            DrawGraphic(Math.Sin, -5, 5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Func<double, double> sel;
+            switch (comboBox1.Text)
+            {
+                case "Sin": sel = Math.Sin;
+                    break;
+                case "x^2": sel = (x) => (Math.Pow(x,2));
+                    break;
+                case "x^3": sel = (x) => (Math.Pow(x, 3));
+                    break;
+                default:
+                    sel = x => x;
+                    break;
+            }
+            var down = int.Parse(textBox1.Text);
+            var up = int.Parse(textBox2.Text);
+            DrawGraphic(sel, down, up);
+        }
+    }
+}
