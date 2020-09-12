edfcb76da94cef68630e3b63a27394267e9f796e
dimadboss <ds-drozdov@yandex.ru>
2020-09-12 11:35:14 +0300

    implemented сonvert function RGB to HSV

--- CompGraphicsLab02/Form4.Designer.cs
+++ CompGraphicsLab02/Form4.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -42,15 +46,38 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(192, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(563, 408);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 356);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form4";
             this.Text = "Задание 3 (Преобразование изображения из RGB в HSV)";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form4_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +85,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- CompGraphicsLab02/Form4.cs
+++ CompGraphicsLab02/Form4.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,103 @@ namespace CompGraphicsLab02
         private void Form4_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+
+        bool Equal(double x, double y, double eps = 0.001)
+        => Math.Abs(x - y) < eps;
+
+        //R, G, B —значения цвета в цветовой модели RGBв диапазоне [0; 1]
+        //MAX—максимум из трёх значений (R, G, B)MIN—минимум из трёх значений (R, G, B)
+        (double H, double S, double V) ConvertRGBtoHSV(double R, double G, double B)
+        {
+            double Max = Math.Max(Math.Max(R, G), B);
+            double Min = Math.Min(Math.Min(R, G), B);
+            double diff = Max - Min;
+
+            // Hue - оттенок
+            double H;
+            if (Equal(Max, Min))
+                H = 0;
+            else
+            if (Equal(Max, R) && G >= B)
+                H = 60 * (G - B) / diff;
+            else
+            if (Equal(Max, R) && G < B)
+                H = 60 * (G - B) / diff + 360;
+            else
+            if (Equal(Max, G))
+                H = 60 * (B - R) / diff + 120;
+            else
+            //if (Equal(Max, B))
+                H = 60 * (R - G) / diff + 240;
+
+            double S = Equal(Max, 0) ? 0 : (1 - Min / Max);
+
+            double V = Max;
+
+            return (H, S, V);
+        }
+
+        (double R, double G, double B) ConvertHSVtoRGB(double H, double S, double V)
+        {
+            double Hi = Math.Floor(H / 60f) % 6;
+            double f = H / 60f - Math.Floor(H / 60f);
+            double p = V * (1 - S);
+            double q = V * (1 - f * S);
+            double t = V * (1 - (1 - f) * S);
+
+            switch (Hi)
+            {
+                case 0:
+                    return (V, t, p);
+                case 1:
+                    return (q, V, p);
+                case 2:
+                    return (p, V, t);
+                case 3:
+                    return (p, q, V);
+                case 4:
+                    return (t, p, V);
+                case 5:
+                    return (V, p, q);
+                default:
+                    return (0, 0, 0);
+            }
+        }
+        /// <summary>
+        /// Тестирование правильность перевода RGB -> HSV
+        /// </summary>
+        void test()
+        {
+            for (int i = 0; i < 100; ++i)
+            {
+                Random rnd = new Random();
+                double R = rnd.NextDouble(), G = rnd.NextDouble(), B = rnd.NextDouble();
+
+                // RGB -> HSV
+                var res = ConvertRGBtoHSV(R, G, B);
+                // HSV -> RGB
+                var res1 = ConvertHSVtoRGB(res.H, res.S, res.V);
+                // Проверяем, что в результате получили исходные значения
+                Debug.Assert(Equal(res1.R, R));
+                Debug.Assert(Equal(res1.G, G));
+                Debug.Assert(Equal(res1.B, B));
+                this.Text = ($"{R} {G} {B}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            test();
+            // Исправить этот колхоз
+            Bitmap myBitmap = new Bitmap(@"c:\Users\Dima\CompGraphicsLabs\CompGraphicsLab02\test.jpg");
+
+            for (int i = 0; i < 10 /*myBitmap.Width*/; ++i)
+                for (int j = 0; j < 10 /*myBitmap.Height*/; ++j)
+                {
+                    //  this.Text = $"{myBitmap.GetPixel(i, j).R}";
+                }
         }
     }
 }
