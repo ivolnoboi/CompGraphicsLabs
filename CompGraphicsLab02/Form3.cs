fb0a0aeb93fc578583729500e0ba8ca0a3c50191
ivolnoboi <irinavolnoboy@gmail.com>
2020-09-12 16:20:22 +0300

    implemented GetRGBChannels function

--- CompGraphicsLab02/Form3.cs
+++ CompGraphicsLab02/Form3.cs
@@ -20,6 +20,7 @@ namespace CompGraphicsLab02
             InitializeComponent();
         }
 
+        // Вернуться в меню (к выбору задания)
         private void button1_Click(object sender, EventArgs e)
         {
             this.Visible = false;
@@ -29,6 +30,72 @@ namespace CompGraphicsLab02
         private void Form3_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        // Открыть изображение
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            // маска для типа файлов
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                    pictureBox2.Image = null;
+                    pictureBox3.Image = null;
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Получение канала R
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap input = new Bitmap(pictureBox1.Image);
+                pictureBox2.Image = GetRgbChannels(input)[0];
+            }
+        }
+
+        static Bitmap[] GetRgbChannels(Bitmap source)
+        {
+            Bitmap[] result = new Bitmap[3] { new Bitmap(source.Width, source.Height), new Bitmap(source.Width, source.Height), new Bitmap(source.Width, source.Height) };
+            for (int i = 0; i < source.Width; i++)
+            {
+                for (int j = 0; j < source.Height; j++)
+                {
+                    Color color = source.GetPixel(i, j);
+                    result[0].SetPixel(i, j, Color.FromArgb(color.A, color.R, 0, 0));
+                    result[1].SetPixel(i, j, Color.FromArgb(color.A, 0, color.G, 0));
+                    result[2].SetPixel(i, j, Color.FromArgb(color.A, 0, 0, color.B));
+                }
+            }
+            return result;
+        }
+        // Получение канала G
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap input = new Bitmap(pictureBox1.Image);
+                pictureBox2.Image = GetRgbChannels(input)[1];
+            }
+        }
+        //Получение канала B
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap input = new Bitmap(pictureBox1.Image);
+                pictureBox2.Image = GetRgbChannels(input)[2];
+            }
         }
     }
 }
