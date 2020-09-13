e3eda1f461029c34491ad5cc2be0be08c8d59461
dimadboss <ds-drozdov@yandex.ru>
2020-09-13 22:54:11 +0300

    Added missing files

---
+++ CompGraphicsLab02/Form5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompGraphicsLab02
+{
+    public partial class Form5 : Form
+    {
+        private Form4 _form4;
+        public Form5(Form4 form4)
+        {
+            _form4 = form4;
+            InitializeComponent();
+        }
+
+        private void Form5_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
