14089731f62f2d229d0969f651722b3729ff9c02
Mikhail <mikes22@yandex.ru>
2020-09-23 19:40:06 +0300

    Done Lab03 Task3

--- CompGraphicsLab03/Form4.Designer.cs
+++ CompGraphicsLab03/Form4.Designer.cs
@@ -29,34 +29,81 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(24, 388);
+            this.button1.Location = new System.Drawing.Point(11, 325);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 37);
+            this.button1.Size = new System.Drawing.Size(80, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "В меню";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(96, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(492, 342);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(11, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(43, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Wu";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(11, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Bresenham";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form4";
             this.Text = "Задание 3";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form4_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
--- CompGraphicsLab03/Form4.cs
+++ CompGraphicsLab03/Form4.cs
@@ -13,10 +13,13 @@ namespace CompGraphicsLab03
     public partial class Form4 : Form
     {
         private Form1 _form1;
+        bool lineStrted = false;
+        Point point;
         public Form4(Form1 form1)
         {
             _form1 = form1;
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -28,6 +31,107 @@ namespace CompGraphicsLab03
         private void Form4_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+        private void DrawLineB(System.Drawing.Pen pen, int x1, int y1, int x2, int y2)
+        {
+            var img = (pictureBox1.Image as Bitmap);
+            var dy = Math.Abs(y2 - y1);
+            var dx = Math.Abs(x2 - x1);
+            int signX = x1 < x2 ? 1 : -1;
+            int signY = y1 < y2 ? 1 : -1;
+            
+            int error = dx - dy;
+            
+            img.SetPixel(x2, y2, pen.Color);
+            pictureBox1.Invalidate();
+            while (x1 != x2 || y1 != y2)
+            {
+                img.SetPixel(x1, y1, pen.Color);
+                pictureBox1.Invalidate();
+                int error2 = error * 2;
+                
+                if (error2 > -dy)
+                {
+                    error -= dy;
+                    x1 += signX;
+                }
+                if (error2 < dx)
+                {
+                    error += dx;
+                    y1 += signY;
+                }
+            }  
+        }
+
+        private void DrawLineWu(System.Drawing.Pen pen, int x1, int y1, int x2, int y2)
+        {
+            float dx = x2 - x1; float dy = y2 - y1;
+            var img = (pictureBox1.Image as Bitmap);
+            if (Math.Abs(dx) > Math.Abs(dy))
+            {
+                if (x1 > x2)
+                {
+                    (x1, x2) = (x2, x1);
+                    (y1, y2) = (y2, y1);
+                }
+                img.SetPixel(x2, y2, pen.Color);
+                pictureBox1.Invalidate();
+
+                float gradient = dy / dx;
+                float y = y1 + gradient;
+                for (var x = x1 + 1; x <= x2 - 1; x++)
+                {
+                    img.SetPixel(x, (int)y, Color.FromArgb((int)((1 - (y - (int)y)) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    img.SetPixel(x, (int)y + 1, Color.FromArgb((int)((y - (int)y) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pictureBox1.Invalidate();
+                    y += gradient;
+                }
+            }
+            else
+            {
+                if (y1 > y2)
+                {
+                    (x1, x2) = (x2, x1);
+                    (y1, y2) = (y2, y1);
+                }
+                img.SetPixel(x2, y2, pen.Color);
+                pictureBox1.Invalidate();
+
+                float gradient = dx / dy;
+                float x = x1 + gradient;
+                for (var y = y1 + 1; y <= y2 - 1; y++)
+                {
+                    img.SetPixel((int)x, y, Color.FromArgb((int)((1 - (x - (int)x)) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    img.SetPixel((int)x + 1, y, Color.FromArgb((int)((x - (int)x) * 255), pen.Color.R, pen.Color.G, pen.Color.B));
+                    pictureBox1.Invalidate();
+                    x += gradient;
+                }
+            }
+        }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lineStrted)
+            {
+                if (checkBox1.Checked)
+                    DrawLineWu(Pens.Black, point.X, point.Y, e.X, e.Y);
+                else
+                    DrawLineB(Pens.Black, point.X, point.Y, e.X, e.Y);
+            }
+            else
+                point = e.Location;
+            lineStrted = !lineStrted;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = !checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = !checkBox2.Checked;
         }
     }
 }
