a5f5d97d3f0517a06e51533945af95caa98185b2
ivolnoboi <irinavolnoboy@gmail.com>
2020-10-03 01:08:42 +0300

    Added adding of primitives

--- CompGraphicsLab04/CompGraphicsLab04/Form1.Designer.cs
+++ CompGraphicsLab04/CompGraphicsLab04/Form1.Designer.cs
@@ -28,13 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(222, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(930, 658);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выбрать примитив";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 111);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(69, 21);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Точка";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 139);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(137, 21);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ребро (отрезок)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 167);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(84, 21);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Полигон";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.radioButton3_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1164, 682);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
--- CompGraphicsLab04/CompGraphicsLab04/Form1.cs
+++ CompGraphicsLab04/CompGraphicsLab04/Form1.cs
@@ -12,9 +12,88 @@ namespace CompGraphicsLab04
 {
     public partial class Form1 : Form
     {
+        private LinkedList<Point> points; // список всех точек на pictureBox
+        private LinkedList<Tuple<Point, Point>> lines; // список всех отрезков на pictureBox
+        private LinkedList<LinkedList<Point>> polygons; // список всех полигонов на pictureBox
+        LinkedList<Point> current; // текущий полигон
+        private Pen pen = new Pen(Color.Black);
+        private Bitmap bmp;
         public Form1()
         {
             InitializeComponent();
+            radioButton1.Checked = true;
+            points = new LinkedList<Point>();
+            lines = new LinkedList<Tuple<Point, Point>>();
+            current = new LinkedList<Point>();
+            polygons = new LinkedList<LinkedList<Point>>();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
         }
+
+        private bool first_point_line = true;
+        private Point first;
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked) // если выбрана точка
+            {
+                points.AddLast(e.Location);
+            }
+            if (radioButton2.Checked) // если выбран отрезок
+            {
+                if (first_point_line)
+                {
+                    first_point_line = false; // говорим, что первая точка уже есть
+                    first = e.Location;
+                }
+                else
+                {
+                    first_point_line = true; // добавляем вторую точку и говорим, что отрезок завершён
+                    lines.AddLast(Tuple.Create(first, e.Location));
+                }
+            }
+            if (radioButton3.Checked)
+            {
+                current.AddLast(e.Location);
+            }
+            DrawPrimitives();
+        }
+
+        private void DrawPrimitives()
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp);
+            foreach (var point in points) // рисуем точки
+            {
+                bmp.SetPixel(point.X, point.Y, Color.Black);
+            }
+            foreach (var point in lines) // рисуем отрезки
+            {
+                g.DrawLine(pen, point.Item1, point.Item2);
+            }
+            foreach (var polygon in polygons)
+            {
+                // рисуем полигон
+                var cur = polygon.First;
+                if (cur != polygon.Last)
+                    g.DrawLine(pen, polygon.First.Value, polygon.Last.Value);
+                while (cur != polygon.Last)
+                {
+                    g.DrawLine(pen, cur.Value, cur.Next.Value);
+                    cur = cur.Next;
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+
+        private void radioButton3_MouseClick(object sender, MouseEventArgs e)
+        {
+            // Не знаю, что с этим делать. Добавляет список точек в список полигонов, а потом удаляет его
+            polygons.AddLast(current);
+            current.Clear(); 
+        }
+
+
+        // radioButton1.Checked - если нажата radioButton1
+        // button1.Enabled - доступна ли кнопка
     }
 }
