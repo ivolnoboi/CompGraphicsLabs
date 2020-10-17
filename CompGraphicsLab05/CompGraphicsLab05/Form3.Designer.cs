b2afda56fd25d24769ba9fcecfb3cd3e9492867f
Mikhail <mikes22@yandex.ru>
2020-10-17 17:23:44 +0300

    Add lab 5 task 2

--- CompGraphicsLab05/CompGraphicsLab05/Form3.Designer.cs
+++ CompGraphicsLab05/CompGraphicsLab05/Form3.Designer.cs
@@ -29,34 +29,185 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.initLLength = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.initRLength = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.NextStep = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.initRoughness = new System.Windows.Forms.TextBox();
+            this.plusBtn = new System.Windows.Forms.Button();
+            this.minusBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initLLength)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initRLength)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(12, 396);
+            this.button1.Location = new System.Drawing.Point(11, 404);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 42);
+            this.button1.Size = new System.Drawing.Size(82, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "В меню";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(518, 425);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(536, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Начальные высоты";
+            // 
+            // initLLength
+            // 
+            this.initLLength.Location = new System.Drawing.Point(581, 25);
+            this.initLLength.Name = "initLLength";
+            this.initLLength.Size = new System.Drawing.Size(61, 20);
+            this.initLLength.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(536, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Левая";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(536, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Правая";
+            // 
+            // initRLength
+            // 
+            this.initRLength.Location = new System.Drawing.Point(581, 51);
+            this.initRLength.Name = "initRLength";
+            this.initRLength.Size = new System.Drawing.Size(61, 20);
+            this.initRLength.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(537, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Шероховатость";
+            // 
+            // NextStep
+            // 
+            this.NextStep.Location = new System.Drawing.Point(539, 125);
+            this.NextStep.Name = "NextStep";
+            this.NextStep.Size = new System.Drawing.Size(102, 23);
+            this.NextStep.TabIndex = 9;
+            this.NextStep.Text = "Следующий шаг";
+            this.NextStep.UseVisualStyleBackColor = true;
+            this.NextStep.Click += new System.EventHandler(this.NextStep_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(540, 414);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(100, 23);
+            this.Clear.TabIndex = 10;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // initRoughness
+            // 
+            this.initRoughness.Location = new System.Drawing.Point(560, 98);
+            this.initRoughness.Name = "initRoughness";
+            this.initRoughness.Size = new System.Drawing.Size(62, 20);
+            this.initRoughness.TabIndex = 11;
+            this.initRoughness.Text = "0,4";
+            // 
+            // plusBtn
+            // 
+            this.plusBtn.Location = new System.Drawing.Point(628, 98);
+            this.plusBtn.Name = "plusBtn";
+            this.plusBtn.Size = new System.Drawing.Size(14, 20);
+            this.plusBtn.TabIndex = 12;
+            this.plusBtn.Text = "+";
+            this.plusBtn.UseVisualStyleBackColor = true;
+            this.plusBtn.Click += new System.EventHandler(this.PlusBtn_Click);
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.Location = new System.Drawing.Point(540, 98);
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.Size = new System.Drawing.Size(14, 20);
+            this.minusBtn.TabIndex = 13;
+            this.minusBtn.Text = "-";
+            this.minusBtn.UseVisualStyleBackColor = true;
+            this.minusBtn.Click += new System.EventHandler(this.minusBtn_Click);
+            // 
             // Form3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(654, 449);
+            this.Controls.Add(this.minusBtn);
+            this.Controls.Add(this.plusBtn);
+            this.Controls.Add(this.initRoughness);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.NextStep);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.initRLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.initLLength);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form3";
             this.Text = "Form3";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form3_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initLLength)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initRLength)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown initLLength;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown initRLength;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button NextStep;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox initRoughness;
+        private System.Windows.Forms.Button plusBtn;
+        private System.Windows.Forms.Button minusBtn;
     }
 }
--- CompGraphicsLab05/CompGraphicsLab05/Form3.cs
+++ CompGraphicsLab05/CompGraphicsLab05/Form3.cs
@@ -12,11 +12,37 @@ namespace CompGraphicsLab05
 {
     public partial class Form3 : Form
     {
+        private class Edge
+        {
+            public PointF left;
+            public PointF right;
+
+            public Edge(PointF _left, PointF _right)
+            {
+                left = _left;
+                right = _right;
+            }
+        }
+
         private Form1 _form1;
+        private Bitmap bmp;
+        Graphics g;
+        List<Edge> edges = new List<Edge>();
+        Random rnd = new Random();
+        double R;
+
         public Form3(Form1 form1)
         {
             _form1 = form1;
             InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(bmp);
+            initLLength.Maximum = pictureBox1.Height;
+            initRLength.Maximum = pictureBox1.Height;
+            initLLength.Value = pictureBox1.Height/2;
+            initRLength.Value = pictureBox1.Height/2;
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -28,6 +54,92 @@ namespace CompGraphicsLab05
         private void Form3_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void NextStep_Click(object sender, EventArgs e)
+        {
+            if (edges.Count == 0)
+            {
+                double lLength;
+                if (!Double.TryParse(initLLength.Text, out lLength))
+                {
+                    lLength = 100;
+                };
+                double rLength;
+                if (!Double.TryParse(initRLength.Text, out rLength))
+                {
+                    rLength = 300;
+                };
+                if (!Double.TryParse(initRoughness.Text, out R))
+                {
+                    R = 0.4;
+                }
+                initLLength.Enabled = false;
+                initRLength.Enabled = false;
+                initRoughness.Enabled = false;
+                Edge first = new Edge(new PointF(0, (float)(bmp.Height - lLength)), new PointF((float)(bmp.Width), (float)(bmp.Height - rLength)));
+                edges.Add(first);
+                DrawEdges();
+            }
+            else
+            {
+                List<Edge> scattered = new List<Edge>();
+                foreach (Edge edge in edges)
+                {
+                    double length = Math.Sqrt((edge.right.X - edge.left.X) * (edge.right.X - edge.left.X) +
+                        (edge.right.Y - edge.left.Y) * (edge.right.Y - edge.left.Y));
+                    double newHeight = (edge.left.Y + edge.right.Y) / 2 + (rnd.NextDouble() - 0.5) * R * length;
+                    PointF middle = new PointF((edge.left.X + edge.right.X) / 2, (int)newHeight);
+                    scattered.Add(new Edge(edge.left, middle));
+                    scattered.Add(new Edge(middle, edge.right));
+                }
+                edges = scattered;
+                DrawEdges();
+
+            }
+        }
+
+        private void DrawEdges()
+        {
+            pictureBox1.Image = bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            foreach (Edge edge1 in edges)
+            {
+                drawEdge(edge1);
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            initLLength.Enabled = true;
+            initRLength.Enabled = true;
+            initRoughness.Enabled = true;
+            edges = new List<Edge>();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            R = 0;
+        }
+        private void drawEdge(Edge edge) => g.DrawLine(Pens.Black, edge.left, edge.right);
+
+        private void PlusBtn_Click(object sender, EventArgs e)
+        {
+            double R_tmp;
+            if (Double.TryParse(initRoughness.Text, out R_tmp))
+            {
+                R_tmp += 0.1;
+                initRoughness.Text = R_tmp.ToString();
+            }
+        }
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            double R_tmp;
+            if (Double.TryParse(initRoughness.Text, out R_tmp))
+            {
+                R_tmp -= 0.1;
+                initRoughness.Text = R_tmp.ToString();
+            }
         }
     }
 }
